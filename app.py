d799a397d040049c5cba695df956bfe24eebdddb
Julio C. <julio.ce01@hotmail.com>
2024-06-25 05:22:03 -0600

    primer commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template, jsonify
+import joblib
+import pandas as pd
+import logging
+
+app = Flask(__name__)
+
+# Configurar el registro
+logging.basicConfig(level=logging.DEBUG)
+
+# Cargar el modelo entrenado
+model = joblib.load('mod23.pkl')
+app.logger.debug('Modelo cargado correctamente.')
+
+@app.route('/')
+def home():
+    return render_template('formulario.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Obtener los datos enviados en el request
+        app.logger.debug(f'Datos recibidos: {request.form}')
+        smoker = float(request.form['smoker'])
+        bmi = float(request.form['bmi'])
+        age = float(request.form['age'])
+        weight = float(request.form['weight'])
+        job_title = float(request.form['job_title'])
+        
+        # Crear un DataFrame con los datos
+        data_df = pd.DataFrame([[smoker, bmi, age, weight, job_title]], columns=['smoker', 'bmi', 'age', 'weight', 'job_title'])
+        app.logger.debug(f'DataFrame creado: {data_df}')
+        
+        # Realizar predicciones
+        prediction = model.predict(data_df)
+        app.logger.debug(f'Predicción: {prediction[0]}')
+        
+        # Devolver las predicciones como respuesta JSON
+        return jsonify({'categoria': prediction[0]})
+    except Exception as e:
+        app.logger.error(f'Error en la predicción: {str(e)}')
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
