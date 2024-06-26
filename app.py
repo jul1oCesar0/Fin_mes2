e59054ef9284e6ea8ab243d0ee2ec8c0002a6f77
Julio C. <julio.ce01@hotmail.com>
2024-06-26 13:14:23 -0600

    cuarto commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 # Cargar el modelo entrenado
-model = joblib.load('mod23.pkl')
+model = joblib.load('mosd.pkl')
 app.logger.debug('Modelo cargado correctamente.')
 
 @app.route('/')
